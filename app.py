dd2e95941e5255842c2a6e0554ad75d48ca2b0b9
chengpeng <106126886+1648027181@users.noreply.github.com>
2022-07-16 18:04:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,37 @@ st.write("")
 option = st.selectbox(
      'Choose the model you want to use?',
      ('resnet50', 'resnet101', 'densenet121','shufflenet_v2_x0_5','mobilenet_v2'))
-file_up = st.file_uploader("Upload an image", type="jpg")
+st.write("")
+st.write("")
+st.subheader("you alse can select some image")
+option2 = st.selectbox(
+     '',
+     ('image_dog', 'image_snake'))
 
-if file_up is not None:
+file_up = st.file_uploader("Upload an image", type="jpg")
+if file_up is None:
+    if option2 =="image_dog":
+        image=Image.open("image/dog.jpg")
+        file_up="image/dog.jpg"
+    else:
+        image=Image.open("image/snake.jpg")
+        file_up="image/snake.jpg"
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
+    st.write("Just a second...")
+    labels, fps = predict(file_up, option)
+
+    # print out the top 5 prediction labels with scores
+    st.success('successful prediction')
+    for i in labels:
+        st.write("Prediction (index, name)", i[0], ",   Score: ", i[1])
+
+    # print(t2-t1)
+    # st.write(float(t2-t1))
+    st.write("")
+    st.metric("", "FPS:   " + str(fps))
+
+else:
     image = Image.open(file_up)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
